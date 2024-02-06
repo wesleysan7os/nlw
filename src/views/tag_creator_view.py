7a67d802d217fd7985405c4a9afd21d256e12bfb
wesleysan7os <wesley.santos.comp@gmail.com>
2024-02-06 13:12:19 -0300

    refactor: view & controller separation

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controller.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
@@ -7,7 +8,9 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
         body = http_request.body
         product_code = body["product_code"]
-        print(product_code)
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        response = tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=200, body=response)
